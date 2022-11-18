1377319a92ed49702704b5b256288f3d1708685f
NiikoViiana <niikov11@gmail.com>
2022-11-18 02:49:55 -0500

    Subida
    Subida

---
+++ jobs.py
@@ -0,0 +1,193 @@
+# For Importing data {Kaggle.}
+import os
+for dirname, _, filenames in os.walk('Dataset'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import regex as re
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+#For Data Viz.
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#For Warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+#For Options
+pd.set_option('display.max_columns' , None)
+pd.set_option('display.max_rows', None)
+
+#From Sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+# For NLP related task
+
+from PIL import Image
+import requests
+
+# Leyendo el DataSet
+filepath = "Dataset/jobss.csv"
+missing_values = ["NaN", " ", "--", "na", "Not Mentioned"]
+data_import = pd.read_csv(filepath , na_values = missing_values)
+data_import.head()
+
+# EDA Exploratory Data Analysis
+# Se elimina la columna que no tiene una definición clara
+df = data_import.drop('Unnamed: 1', axis = 1)
+df.head()
+# Se copia el dataset sin la columna
+df1 = df.copy()
+# Se verifican los tipos de objetos de cada una de las columnas
+df.info()
+
+# Se verifican los datos vacios del DataSet
+def missing_vals(df):
+    for i in df:
+        print(f"{i}:{df[i].isnull().sum()} de {len(df[i])} datos")
+
+missing_vals(df)
+
+# Se identifican las columnas que son de tipo categorico
+def cat_cols(df) :
+    o = (df.dtypes == 'object')
+    object_cols = o[o].index
+    return object_cols
+
+object_cols = cat_cols(df)
+print(f"Las columnas categoricas son : {object_cols}")
+
+# Se identifican los posibles numeros que existan en las columnas categoricas
+def detecting_num_in_cat_columns(df):
+    cnt=0
+    for row in df:
+        if type(row) == 'object' :
+            try:
+                int(row)
+                df.loc[cnt, row] = np.nan
+            except ValueError:
+                pass
+            cnt+=1
+    return f"Existen {cnt} numeros en las columnas categoricas."
+
+print(detecting_num_in_cat_columns(df))
+
+# Se toman los años de experiencia sin repetir
+print(df['Job Experience Required'].unique())
+# Se limpian los datos nulos y datos que no corresponden
+df['Job Experience Required'] = df['Job Experience Required'].replace('vide', 0)
+df['Job Experience Required'] = df['Job Experience Required'].replace(np.nan, 0)
+print(df['Job Experience Required'].unique())
+# Se verifica si existe algun dato nullo
+print(f"Datos nulos: {df['Job Experience Required'].isnull().sum()}" )
+
+# Detectando el total de la experiencia requerida
+experience_required = []
+numbers_list = []
+
+def experience_required_func(df):
+    for i in df['Job Experience Required']:
+        if type(i) == str:
+            if re.search(r'\d+', i):
+                numbers = re.findall(r'\d+', i)
+                numbers_list.append(numbers)
+        else:
+            numbers_list.append([0])
+
+    for number in numbers_list:
+
+        if len(number) != 1:
+            first_num = int(number[0])
+            second_num = int(number[1])
+            sub = second_num - first_num
+            experience_required.append(abs(sub))
+        else:
+            experience_required.append(0)
+
+    return experience_required
+
+experience_required = experience_required_func(df)
+
+print(len(experience_required))
+
+#Visualización de los datos
+# Porcentaje de trabajos con mas experiencia
+data_viz_df = df.copy()
+data_viz_df['Experiencia en años'] = experience_required
+data_viz_df.head()
+# Graficamente
+data_viz_df['Experiencia en años'].value_counts()[:5].plot(kind = 'pie', autopct = '%1.1f%%', shadow = True, explode = [0.1, 0, 0, 0, 0])
+plt.title('Porcentaje de trabajos con mas experiencia')
+fig = plt.gcf()
+fig.set_size_inches(7, 7)
+plt.show()
+# Se puede cocluir que casi el 34% de las personas tienen una experiencia de 5 años en el dataset
+# Tmbien el 23,1% de las personas tiene una experiencia de 2 años en el dataset
+
+# Visualización de los datos
+# Tipos comunes de trabajos
+data_viz_df['Tipos comunes de trabajos'] = data_viz_df['Job Title']
+
+def job_types(data):
+    for i in data:
+        if type(i) == str:
+            if 'developer' in i.lower():
+                data_viz_df['Tipos comunes de trabajos'].replace(to_replace=i.lower(), value='Desarrolladores')
+
+            elif 'analyst' in i.lower():
+                data_viz_df['Tipos comunes de trabajos'].replace(to_replace=i.lower(), value='Analistas')
+
+            elif 'sales' in i.lower():
+                data_viz_df['Tipos comunes de trabajos'].replace(to_replace=i.lower(), value='Vendedores')
+        else:
+            data_viz_df['Tipos comunes de trabajos'].replace(i, 'Otros')
+
+    return data_viz_df['Tipos comunes de trabajos']
+
+data_viz_df['Tipos comunes de trabajos'] = job_types(data_viz_df['Job Title'])
+
+common_jobs = []
+for i in data_viz_df['Job Title']:
+    if type(i) == str:
+        if 'developer' in i.lower():
+            common_jobs.append('Desarrolladores')
+        elif 'analyst' in i.lower():
+            common_jobs.append('Analistas')
+        elif 'sales' in i.lower():
+            common_jobs.append('Vendedores')
+        elif 'data scientist' in i.lower():
+            common_jobs.append('Cientificos de datos')
+        elif 'manager' in i.lower():
+            common_jobs.append('Gerentes')
+        elif 'media' in i.lower():
+            common_jobs.append('Medias')
+        elif 'support' in i.lower():
+            common_jobs.append('Soporte')
+        elif 'engineer' in i.lower():
+            common_jobs.append('Ingenieros')
+        elif 'bde' in i.lower():
+            common_jobs.append('BDE')
+        elif 'ui' or 'ux' in i.lower():
+            common_jobs.append('Diseño')
+
+    else :
+        common_jobs.append('Otros')
+
+data_viz_df['Tipos comunes de trabajos'] = common_jobs
+
+data_viz_df = data_viz_df.drop(['Role', 'Industry'] , axis = 1)
+
+data_viz_df['Tipos comunes de trabajos'].value_counts().plot(kind = 'pie', autopct = '%1.1f%%', shadow = True, explode = [0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+plt.title('Tipos comunes de trabajos')
+fig = plt.gcf()
+fig.set_size_inches(7, 7)
+plt.show()
+
+# Visualización de resultados
+print(data_viz_df[['Experiencia en años', 'Tipos comunes de trabajos', 'sal']]\
+    .groupby(['Experiencia en años', 'Tipos comunes de trabajos'])\
+    .mean().sort_values(by = 'Experiencia en años', ascending = False))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load
+import sys
 
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -12,39 +13,89 @@ import gc
 from tqdm.notebook import tqdm
 from mpl_toolkits.basemap import Basemap
 
+from sklearn.linear_model import LinearRegression
+
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_score, train_test_split, KFold
+from sklearn.metrics import roc_auc_score, mean_squared_error, confusion_matrix, mean_absolute_error
+from bayes_opt import BayesianOptimization
+import scikitplot as skplt
+import lightgbm as lgb
+
 warnings.filterwarnings('ignore')
 
-data = pd.read_csv("Dataset/jobss.csv")
-print("\n Quick view of the data: \n")
-# data.head()
-# data.describe()
-# data.info()
+# EDA Exploratory Data Analysis
+originalData = pd.read_csv("Dataset/jobss.csv")
+originalData.head()
+originalData.info()
 
-def mapWorld():
-    m = Basemap(projection='mill', llcrnrlat=-60, urcrnrlat=90, \
-                llcrnrlon=-180, urcrnrlon=180, resolution='c')
-    # m.drawcoastlines()
-    m.drawcountries()
-    m.drawparallels(np.arange(-90, 91., 30.))
-    m.drawmeridians(np.arange(-90, 90., 60.))
+# De la data original se identifica la cantidad de Features que existen
+# con su tipo de dato, verificando el contexto del analisis, se determina que
+# algunas features no son necesarias para la predicción del Job Title
+# por lo que se realiza una manipulación de la data, para dejar solo el Job Title & las Key Skills
+# tambien se realiza una agrupacion por Industry para sacar los Job Title mas relevantes
+# a su vez tambien se identifica que existen datos nulos y con caracteres peligrosos
+# por lo que se realiza una limpieza de datos
 
-    m.drawmapboundary(fill_color='#FFFFFF')
-    lat = data['Latitude'].values
-    lon = data['Longitude'].values
-    a_1 = data['sal'].values
-    a_2 = data['Counts'].values
-    m.scatter(lon, lat, latlon=True, c=100 * a_1, s=20 * a_2, linewidth=0, edgecolors='black', cmap='hot', alpha=1)
+# Limpieza de los datos
+cleanData = pd.read_csv("Dataset/cleanjobs.csv")
 
-    m.fillcontinents(color='#072B57', lake_color='#FFFFFF', alpha=0.4)
-    cbar = m.colorbar()
-    cbar.set_label('Location counts and sal')
-    # plt.clim(20000, 100000)
-    plt.title("Location counts and sal", fontsize=30)
+# print("\n Quick view of the data: \n")
+cleanData.head()
+
+cleanData.Key_Skills.info()
+
+newSkills = []
+
+for skills in cleanData.Key_Skills:
+    arraySkills = skills.split('|')
+
+    for skill in arraySkills:
+        newSkills.append(skill)
+
+idx = pd.Index(newSkills, name='Key_Skills')
+
+# Se verifica que
+idx.value_counts(normalize=True)
+idx.value_counts(normalize=True).plot.pie()
+plt.show()
+
+# Gracias a una grafica, se puede observar la recurrencia de años que exigen
+# Cada uo de los datos
+cleanData.Job_Experience_Required.value_counts(normalize=True)
+cleanData.Job_Experience_Required.value_counts(normalize=True).plot.pie()
+plt.show()
+
+
+# Entrenamiento del modelo
+y = cleanData["ID"]
+del cleanData["ID"]
+gc.collect()
+
+X_train, X_val, y_train, y_val = train_test_split(cleanData, y, test_size = 0.2, random_state=42)
+
+def evaluateRegressor(true, predicted, message="Test set"):
+    MSE = mean_squared_error(true, predicted, squared=True)
+    MAE = mean_absolute_error(true, predicted)
+    RMSE = mean_squared_error(true, predicted, squared=False)
+    LogRMSE = mean_squared_error(np.log(true), np.log(predicted), squared=False)
+    print(message)
+    print("MSE:", MSE)
+    print("MAE:", MAE)
+    print("RMSE:", RMSE)
+    print("LogRMSE:", LogRMSE)
+
+
+def PlotPrediction(true, predicted, title="Dataset: "):
+    fig = plt.figure(figsize=(20, 20))
+    ax1 = fig.add_subplot(111)
+    ax1.set_title(title + 'True vs Predicted')
+    ax1.scatter(list(range(0, len(true))), true, s=10, c='r', marker="o", label='True')
+    ax1.scatter(list(range(0, len(predicted))), predicted, s=10, c='b', marker="o", label='Predicted')
+    plt.legend(loc='upper right');
     plt.show()
 
+LGBMReg = lgb.LGBMRegressor().fit(X_train, y_train)
+pred = LGBMReg.predict(X_val)
 
-sns.set(style="white", font_scale=1.5)
-plt.figure(figsize=(30, 30))
-
-data["Counts"] = data["Location"].map(data["Location"].value_counts())
-mapWorld()
+evaluateRegressor(y_val, pred, "Train Set:")
+PlotPrediction(y_val, pred, "Train Set: ")
